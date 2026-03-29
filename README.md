eaf5f11deb7cf7bf2117d6c13e70fea8ff7ae419
Silent <zdanio95@gmail.com>
2026-03-29 22:56:02 +0200

    GTA2 support

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Extractor for GXT files from GTA games.
 
 Supported games and encodings:
+* GTA2 (UTF-16)
 * GTA III (UTF-16)
 * GTA Vice City (UTF-16)
 * GTA San Andreas (ASCII and UTF-16)
--- gta/gxt.py
+++ gta/gxt.py
@@ -1,7 +1,16 @@
 import struct
+from abc import ABC, abstractmethod
 from collections import OrderedDict
 
-class III:
+class DecoderBase(ABC):
+    @abstractmethod
+    def readData(self, file, charmap):
+        pass
+
+    def extendCharmap(self, charmap):
+        return charmap
+
+class III(DecoderBase):
     def readData(self, file, charmap):
         data = OrderedDict()
         data['MAIN'] = self._readOneTable(file, charmap)
@@ -22,8 +31,8 @@ class III:
 
         for entry in TKey:
             key = entry[1]
-            value_data = TDat[entry[0]:].split(b'\x00\x00', 1)[0]
-            value = ''.join(charmap[value_data[i]] for i in range(0, len(value_data), 2))
+            value_data = _trimString(TDat[entry[0]:], 2)
+            value = ''.join(charmap[int.from_bytes(value_data[i:i+2], byteorder='little')] for i in range(0, len(value_data), 2))
             tabl_entries[key.split(b'\x00', 1)[0].decode()] = value
 
         return tabl_entries
@@ -38,10 +47,25 @@ class VC(III):
 
         return data
 
-class SA:
+class GTA2(III):
+    def extendCharmap(self, charmap):
+        charmap[0x216B] = '{Krishna} '
+        charmap[0x216C] = '{Loony} '
+        charmap[0x216D] = '{Russian} '
+        charmap[0x216E] = '{Neutral} '
+        charmap[0x2170] = '{Police} '
+        charmap[0x2172] = '{Redneck} '
+        charmap[0x2173] = '{Scientist} '
+        charmap[0x2179] = '{Yakuza} '
+        charmap[0x217A] = '{Zaibatsu} '
+
+        # This is broken, but was intended to be Scientist
+        charmap[0x2153] = '{???} '
+        return charmap
+
+class SA(DecoderBase):
     def __init__(self, char_size):
         self.char_size = char_size
-        self.term_char = b'\x00' * char_size
 
     def readData(self, file, charmap):
         data = OrderedDict()
@@ -61,8 +85,8 @@ class SA:
 
             for entry in TKey:
                 key = f'0x{entry[1]:08X}'
-                value_data = TDat[entry[0]:].split(self.term_char, 1)[0]
-                value = ''.join(charmap[value_data[i]] for i in range(0, len(value_data), self.char_size))
+                value_data = _trimString(TDat[entry[0]:], self.char_size)
+                value = ''.join(charmap[int.from_bytes(value_data[i:i+self.char_size], byteorder='little')] for i in range(0, len(value_data), self.char_size))
                 tabl_entries[key] = value
 
             data[table[0]] = tabl_entries
@@ -70,13 +94,15 @@ class SA:
         return data
 
 def readGxtFile(gxt_path, charmap_path):
-    charmap = ['\x00'] * 32
+    charmap_list = []
     with open(charmap_path, 'r', encoding='utf-8') as f:
         for line in f:
-            charmap.extend(line.rstrip('\r\n').split('\t'))
+            charmap_list.extend(line.rstrip('\r\n').split('\t'))
+    charmap = {index + 32: value for index, value in enumerate(charmap_list)}
 
     with open(gxt_path, 'rb') as gxt:
         gxt_version, reader = _getReader(gxt)
+        charmap = reader.extendCharmap(charmap)
         return gxt_version, reader.readData(gxt, charmap)
 
 # Internal functions
@@ -98,6 +124,15 @@ def _parseTables(stream):
 
     return Tables
 
+def _trimString(string, char_size):
+    term = b'\x00' * char_size
+    for i in range(0, len(string) + 1 - char_size, char_size):
+        if string[i:i+char_size] == term:
+            string = string[:i]
+            break
+
+    return string
+
 def _getReader(file):
     bytes = file.peek(8) [:8]
 
@@ -109,9 +144,15 @@ def _getReader(file):
         if word2 == 16:
             return 'gtasa-mobile', SA(2)
 
+    # III/VC
     if bytes[:4] == b'TABL':
         return 'gtavc', VC()
     elif bytes[:4] == b'TKEY':
         return 'gtaiii', III()
+
+    # GTA2
+    magic, language, version = struct.unpack('<3scH', bytes[:6])
+    if magic == b'GBL' and version == 100:
+        return f'gta2 ({language.decode('ascii')})', GTA2()
 
     return 'unknown', None
