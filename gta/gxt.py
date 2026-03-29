607f4cf8762dba05a543ba395c82a18477ac27a5
Silent <zdanio95@gmail.com>
2026-03-29 18:56:49 +0200

    Add charmap support
    Fixes #2

--- gta/gxt.py
+++ gta/gxt.py
@@ -1,35 +1,37 @@
 import struct
 import os
 
-class VC:    
+class VC:
     def hasTables(self):
         return True
 
     def parseTables(self, stream):
         return _parseTables(stream)
 
-    def parseTKeyTDat(self, stream):
-        size = findBlock(stream, 'TKEY')
-        
+    def parseTKeyTDat(self, stream, charmap):
+        size = findBlock(stream, b'TKEY')
+
         TKey = []
         for i in range(int(size / 12)): # TKEY entry size - 12
             TKey.append( struct.unpack('<I8s', stream.read(12)) )
-        
-        datSize = findBlock(stream, 'TDAT')
+
+        datSize = findBlock(stream, b'TDAT')
         TDat = stream.read(datSize)
 
         Entries = []
 
         for entry in TKey:
             key = entry[1]
-            value = TDat[entry[0]:].decode('utf-16').split('\x00', 1) [0]
-            Entries.append( (key.split(b'\x00')[0].decode(), value) ) # TODO: charmap
-        
+            value_data = TDat[entry[0]:].split(b'\x00\x00', 1)[0]
+            value = ''.join(charmap[value_data[i]] for i in range(0, len(value_data), 2))
+            Entries.append( (key.split(b'\x00', 1)[0].decode(), value) )
+
         return Entries
 
 class SA:
-    def __init__(self, encoding):
-        self.encoding = encoding
+    def __init__(self, char_size):
+        self.char_size = char_size
+        self.term_char = b'\x00' * char_size
 
     def hasTables(self):
         return True
@@ -37,29 +39,30 @@ class SA:
     def parseTables(self, stream):
         return _parseTables(stream)
 
-    def parseTKeyTDat(self, stream):
-        size = findBlock(stream, 'TKEY')
-        
+    def parseTKeyTDat(self, stream, charmap):
+        size = findBlock(stream, b'TKEY')
+
         TKey = []
         for i in range(int(size / 8)): # TKEY entry size - 8
             TKey.append( struct.unpack('<II', stream.read(8)) )
-        
-        datSize = findBlock(stream, 'TDAT')
+
+        datSize = findBlock(stream, b'TDAT')
         TDat = stream.read(datSize)
 
         Entries = []
 
         for entry in TKey:
             key = f'0x{entry[1]:08X}'
-            value = TDat[entry[0]:].decode(self.encoding).split('\x00', 1) [0]
+            value_data = TDat[entry[0]:].split(self.term_char, 1)[0]
+            value = ''.join(charmap[value_data[i]] for i in range(0, len(value_data), self.char_size))
 
-            Entries.append( (key, value) ) # TODO: charmap
-        
+            Entries.append( (key, value) )
+
         return Entries
 
 
 def findBlock(stream, block):
-    while stream.peek(4) [:4] != block.encode():
+    while stream.peek(4) [:4] != block:
         stream.seek(1, os.SEEK_CUR)
 
     _, size = struct.unpack('<4sI', stream.read(8))
@@ -71,33 +74,41 @@ def getVersion(stream):
 
     # SA
     word1, word2 = struct.unpack('<HH', bytes[:4])
-    if word1 == 4 and bytes[4:] == 'TABL'.encode():
+    if word1 == 4 and bytes[4:] == b'TABL':
         if word2 == 8:
             return 'sa'
         if word2 == 16:
             return 'sa-mobile'
-    
-    if bytes[:4] == 'TABL'.encode():
+
+    if bytes[:4] == b'TABL':
         return 'vc'
-    
+
     return None
 
 def getReader(version):
     if version == 'vc':
         return VC()
     if version == 'sa':
-        return SA('cp1252')
+        return SA(1)
     if version == 'sa-mobile':
-        return SA('utf-16')
+        return SA(2)
     return None
+
+def readCharmap(path):
+    entries = ['\x00'] * 32
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f:
+            entries.extend(line.rstrip('\r\n').split('\t'))
+
+    return entries
 
 # Internal functions
 def _parseTables(stream):
-    size = findBlock(stream, 'TABL')
+    size = findBlock(stream, b'TABL')
     Tables = []
-        
+
     for i in range(int(size / 12)): # TABL entry size - 12
         rawName, offset = struct.unpack('<8sI', stream.read(12))
-        Tables.append( (rawName.split(b'\x00')[0].decode(), offset) )
-    
+        Tables.append( (rawName.split(b'\x00', 1)[0].decode(), offset) )
+
     return Tables
--- gxt-extract.py
+++ gxt-extract.py
@@ -1,37 +1,49 @@
+import argparse
 import sys
 import os
 import gta.gxt
 
-args = sys.argv[1:]
-outDirName = os.path.splitext(args[0])[0]
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
-def readOutTable(gxt, reader, name):
-    os.makedirs(os.path.join(outDirName, name), exist_ok=True)
+parser = argparse.ArgumentParser(
+    description="A tool for extracting the GXT files from the GTA games.")
 
-    with open(os.path.join(outDirName, name, name + '.txt'), 'w', encoding='utf-8') as f:
-        for text in reader.parseTKeyTDat(gxt):
+parser.add_argument('gxt', help='path to the GXT file to extract')
+parser.add_argument('-c', '--charmap', default='charmap.txt', help='path to the character map file (default: %(default)s)')
+
+args = parser.parse_args()
+
+outDirName = os.path.splitext(args.gxt)[0]
+def readOutTable(gxt, charmap, reader, name):
+    with open(os.path.join(outDirName, name + '.txt'), 'w', encoding='utf-8') as f:
+        for text in reader.parseTKeyTDat(gxt, charmap):
             f.write( text[0] + '\t' + text[1] + '\n' )
 
-
-# TODO: Parse arguments
-with open(args[0], 'rb') as gxt:
+with open(args.gxt, 'rb') as gxt:
     gxtversion = gta.gxt.getVersion(gxt)
 
     if not gxtversion:
-        print('Unknown GXT version!', file=sys.stderr)
+        eprint('Unknown GXT version!')
         exit(1)
 
-    print("Detected GXT version: {}".format(gxtversion))
+    print(f"Detected GXT version: {gxtversion}")
+
+    charmap = gta.gxt.readCharmap(args.charmap)
+    if not charmap:
+        eprint('Invalid character map! It must be a file with 256 tab-delimited entries')
+        exit(1)
 
     gxtReader = gta.gxt.getReader(gxtversion)
-    
+
     Tables = []
     if gxtReader.hasTables():
         Tables = gxtReader.parseTables(gxt)
 
-    readOutTable(gxt, gxtReader, 'MAIN')
+    os.makedirs(outDirName, exist_ok=True)
+    readOutTable(gxt, charmap, gxtReader, 'MAIN')
 
     if Tables:
         for t in Tables[1:]:
             gxt.seek(t[1])
-            readOutTable(gxt, gxtReader, t[0])
+            readOutTable(gxt, charmap, gxtReader, t[0])
